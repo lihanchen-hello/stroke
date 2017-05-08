b58f4463de94a981ac954872b85d48375f5c313c
lihanchen-hello <lihanchen@tju.edu.cn>
2017-05-08 23:18:47 +0800

    Update NCBars.cpp

--- NCBars.cpp
+++ NCBars.cpp
@@ -1,1 +1,55 @@
+#include"NCBars"
 
+NCBars::NCBars(vector<int> &time,vector<float> &high, vector<float> &low)
+{
+int tn=time.size();
+int hn=high.size();
+int ln=low.size();
+
+if(tn!=hn||tn!=ln)
+{
+cout<<"numbers error"<endl;
+return;
+}
+
+else 
+{
+for(int i=0;i<tn;i++)
+{
+NCBars tmp;
+tmp.time=time[i];
+tmp.high=high[i];
+tmp.low=low[i];
+ncbars.push_back(tmp);
+}
+
+}
+}
+
+
+NCBars::NCBars(NCBars orgNCBars)
+{
+orgNCBars.getAllNCBars(this–>ncbars);
+
+}
+
+void NCBars::operator =(NCBars orgNCBars)
+{
+orgNCBars.getAllNCBars(this–>ncbars);
+}
+
+int size()
+{
+return ncbars.size();
+}
+
+bool NCBars::getAllNCBars(vector<NCBar> &outNCBar)
+{
+outNCBar=this–>ncbars;
+return true;
+}
+
+NCBar NCBar::at(int p)
+{
+return ncbars[p];
+}
